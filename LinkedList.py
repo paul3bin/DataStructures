aa9ecc1d3108f031c88d5e829368e7474c6f5333
Ebin Paul <ebinpaul@outlook.com>
2020-07-14 11:44:37 +0530

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,3 +1,7 @@
+'''
+Code for Singly Linked List
+'''
+
 # Class for Node
 class Node:
 
@@ -23,29 +27,50 @@ class LinkedList:
             while(lastNode.next is not None):
                 lastNode = lastNode.next
             lastNode.next = newNode
-    
-    def getCount(self):
+
+    def delete(self):
         temp = self.head
-        count=0
-        while(temp):
-            count+=1
-            temp = temp.next
-        return count
+        if temp is None:
+            print('Cannot delete from an empty list.')
+        else:
+            value = int(input('Enter the value to be removed: '))
+            if temp.data == value:
+                self.head = temp.next
+                temp = None
+                return
+            else:
+                while temp is not None:
+                    if temp.data==value:
+                        break
+                    prev = temp
+                    temp = temp.next
+
+                if temp is None:
+                    print('Value not found!')
+                    return
+                prev.next = temp.next
+                temp = None
 
     def printLinkedList(self):
         temp = self.head
-        while(temp):
+        while(temp is not None):
             print(temp.data)
             temp = temp.next
+
 response = 'y'
 LLobject = LinkedList()
+
 while response=='y':
     print('\n----------------------Linked List----------------------')
-    print('\n1. Insert\n2. Print linked list')
+    print('\n1. Insert\n2. Delete value\n3. Print linked list')
     choice = int(input('Enter your choice...'))
     if choice==1:
-        data = input('Enter data(integer)....')
+        data = int(input('Enter data(integer)....'))
         LLobject.insert(data)
     elif choice==2:
+        LLobject.delete()
+    elif choice==3:
         print(LLobject.printLinkedList())
+    else:
+        print('Not a valid option. Try again.')
     response = input('Want to continue? (y/n)....')
