18f85ca83ce21cd0a7138ec0334fa7e330e11e14
Ebin Paul <ebinpaul@outlook.com>
2021-04-01 06:53:53 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,34 +1,39 @@
 """
 Python code for hashing function
 """
+
+
 def search(x):
-    if x>0:
-        if has[x][0]==1:
+    if x > 0:
+        if has[x][0] == 1:
             return True
-#if X is negative take the absolute value of x.
-    x=abs(x)
-    if has[x][1]==1:
+# if X is negative take the absolute value of x.
+    x = abs(x)
+    if has[x][1] == 1:
         return True
     return False
 
-def insert(a,n):
-    for i in range(0,n):
-        if a[i]>=0:
-            has[a[i]][0]=1
+
+def insert(a, n):
+    for i in range(0, n):
+        if a[i] >= 0:
+            has[a[i]][0] = 1
         else:
-            has[abs(a[i])][1]=1
+            has[abs(a[i])][1] = 1
 
-a=[-1,9,-5,-8,-5,-2]
-n=len(a)
 
-max=1000
+if __name__ == '__main__':
+    a = [-1, 9, -5, -8, -5, -2]
+    n = len(a)
 
-has = [[0 for i in range(2)]
-        for j in range(max+1)]
-insert(a,n)
+    max = 1000
 
-x=10
-if search(x) is True:
-    print('\nPresent\n')
-else:
-    print('\nNot present\n')
+    has = [[0 for i in range(2)]
+           for j in range(max+1)]
+    insert(a, n)
+
+    x = 10
+    if search(x) is True:
+        print('\nPresent\n')
+    else:
+        print('\nNot present\n')
